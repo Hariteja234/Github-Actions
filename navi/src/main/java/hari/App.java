34fe14162237b4115f91c2bd0574b180b8bc5214
= <=>
2025-05-05 22:03:55 +0530

    updated

--- navi/src/main/java/hari/App.java
+++ navi/src/main/java/hari/App.java
@@ -1,20 +1,8 @@
-public class PrintPattern {
+public class HelloWorld {
 
  public static void main(String[] args) {
 
- int rows = 5;
-
- for (int i = 1; i <= rows; ++i) {
-
- for (int j = 1; j <= i; ++j) {
-
- System.out.print(“* “);
-
- }
-
- System.out.println();
-
- }
+ System.out.println(“Hello, World!”);
 
  }
 
