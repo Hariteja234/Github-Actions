6c6ace12b1de042f6e36907618a701dc3c6d68e9
= <=>
2025-05-09 11:13:14 +0530

    file updated

--- navi/src/main/java/hari/HelloWorld.java
+++ navi/src/main/java/hari/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        String name = args.length > 0 ? args[0] : "world";
+        System.out.println("Hello, " + name + "!");
     }
 }
+
